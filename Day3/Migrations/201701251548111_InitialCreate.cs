d72606536df312648ab351da98db17c86616f6ef
austincrisp <austi@AUSTINBLADE>
2017-01-25 12:12:03 -0500

    Implemented Movie/Review Database using Entity Framework and discussed Migrations

---
+++ Day3/Migrations/201701251548111_InitialCreate.cs
@@ -0,0 +1,62 @@
+namespace Day3.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Movies",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                        Genre = c.String(),
+                        ReleaseDate = c.DateTime(nullable: false),
+                        IMDBUrl = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.People",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Occupation = c.String(),
+                        Gender = c.String(),
+                        City = c.String(),
+                        Age = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Reviews",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Rating = c.Int(nullable: false),
+                        Movie_Id = c.Int(),
+                        Person_Id = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Movies", t => t.Movie_Id)
+                .ForeignKey("dbo.People", t => t.Person_Id)
+                .Index(t => t.Movie_Id)
+                .Index(t => t.Person_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Reviews", "Person_Id", "dbo.People");
+            DropForeignKey("dbo.Reviews", "Movie_Id", "dbo.Movies");
+            DropIndex("dbo.Reviews", new[] { "Person_Id" });
+            DropIndex("dbo.Reviews", new[] { "Movie_Id" });
+            DropTable("dbo.Reviews");
+            DropTable("dbo.People");
+            DropTable("dbo.Movies");
+        }
+    }
+}
---
+++ Day3/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new MovieReviewContext())
+            {
+                Movie movie = new Movie
+                {
+                    Title = "Days of Thunder",
+                    Genre = "Action",
+                    ReleaseDate = DateTime.Now
+                };
+
+                Person myPerson = new Person
+                {
+                    Occupation = "Race Car Driver",
+                    Age = 30
+                };
+
+                Review myReview = new Review
+                {
+                    Person = myPerson,
+                    Movie = movie,
+                    Rating = 7
+                };
+
+                db.Movies.Add(movie);
+                db.Persons.Add(myPerson);
+                db.Reviews.Add(myReview);
+                db.SaveChanges();
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+            /*
+            Person austin = new Person { Occupation = "Developer", Gender = "M", City = "Greenville", Age = 25 };
+            Person bill = new Person { Occupation = "Developer", City = "Barcelona" };
+
+            Movie littleShop = new Movie
+            {
+                Title = "Little Shop of Horrors",
+                Genre = "Musical",
+                ReleaseDate = DateTime.Now
+            };
+
+            Review myReview = new Review
+            {
+                Person = austin,
+                Movie = littleShop,
+                Rating = 8
+            };
+
+            Review billReview = new Review
+            {
+                Person = bill,
+                Movie = littleShop,
+                Rating = 3
+            };
+
+            Console.WriteLine(billReview);
+            Console.WriteLine(myReview);
+            */
+        }
+    }
+}
